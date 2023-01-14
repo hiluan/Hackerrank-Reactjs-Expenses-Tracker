18585e38fe82a50529bb73f4698ac89532571767
hiluan <apt.muenchen@gmail.com>
2023-01-13 19:59:36 -0800

    remove independencies

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import { applyPolyfills, defineCustomElements } from "h8k-components/loader";
+// import registerServiceWorker from "./registerServiceWorker";
+// import { applyPolyfills, defineCustomElements } from "h8k-components/loader";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 // registerServiceWorker();
